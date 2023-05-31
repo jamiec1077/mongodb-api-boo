ce61d1a4185222cbdab4ae83e4d4a0daac5799c2
jamiec1077 <jamie.charlemagne1992@gmail.com>
2023-05-31 07:32:38 -0400

    filled out my routes folder

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Require Users and Thoughts Models
+const Thoughts = require('./thought');
+const Users = require('./user');
+
+// Export Modules: Users and Thoughts
+module.exports = { Users, Thoughts };
--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-
 const express = require('express');
-
-
 const db = require('./config/connection');
-const { urlencoded } = require('body-parser');
+const routes = require('./routes');
 
-s
-const PORT = process.env.PORT|| 3001;
+const cwd = process.cwd();
 
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-const app = (express());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
 
-
-app.use(express.urlencoded({extended: true}));
-
-app.use(express.json);
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
